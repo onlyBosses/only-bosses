709ce067f2659b4a71a7e8623cab7466a400dede
kangkyuchang <70442542+kangkyuchang@users.noreply.github.com>
2025-05-01 18:08:32 +0900

    플레이어 기본 이동 로직 구현
    방향 전환, 좌우 이동, 점프

---
+++ only-bosses/Assets/Script/Move_Player.cs
@@ -0,0 +1,60 @@
+using UnityEditor.Rendering.LookDev;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class Move_Player : MonoBehaviour
+{
+    
+    private float vx;
+    private bool isJump;
+    private bool isJumpPush;
+    private Rigidbody2D rbody;
+    void Start()
+    {
+        rbody = this.GetComponent<Rigidbody2D>();
+        rbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+        vx = 0;
+        isJumpPush = false;
+        isJump = false;
+    }
+
+    void Update()
+    {
+        vx = 0;
+        if(Input.GetKey("a")) {
+            vx = -1;
+        }
+
+        if(Input.GetKey("d")) {
+             vx = 1;
+        }
+
+        if(Input.GetKey("w")) {
+            if(!isJumpPush) {
+                if(Physics2D.Raycast(transform.position, Vector3.down, 1F, LayerMask.GetMask("Ground"))) {
+                    isJump = true;
+                }
+                isJumpPush = true;
+            }
+        }
+        else {
+            isJumpPush = false;
+        }
+
+        if(Input.GetKey("s")) {
+
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        this.GetComponent<SpriteRenderer>().flipX = worldPos.x < this.transform.position.x;
+        rbody.linearVelocity = new Vector2(vx, rbody.linearVelocityY);
+        if(isJump) {
+            isJump = false;
+            rbody.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+        }
+    }
+}
